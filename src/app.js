fb5e3fe1a103c0770e928c352debca81047b5b3d
Faiz Ali Khan <fk00750@yahoo.com>
2024-05-23 19:42:11 +0530

    cors

--- src/app.js
+++ src/app.js
@@ -65,7 +65,7 @@ app.use(helmet({
 /**
  * Define allowed origins for CORS (Cross-Origin Resource Sharing) policy.
  */
-const allowedOrigins = ["localhost:4001"];
+const allowedOrigins = ["https://authenticate-kx0v.onrender.com/"];
 app.use(
     cors({
         origin: (origin, callback) => {
