40a2b70766a6ed6e0e53559a6aba5c4e567a1b5d
Faiz Ali Khan <fk00750@yahoo.com>
2024-05-24 15:47:07 +0530

    cors

--- src/app.js
+++ src/app.js
@@ -52,7 +52,7 @@ const app = express();
 const allowedOrigins = ["https://authenticate-kx0v.onrender.com"];
 const corsOptions = {
     origin: function (origin, callback) {
-        if (allowedOrigins.includes(origin)) {
+        if (!origin || allowedOrigins.includes(origin)) {
             callback(null, true)
         } else {
             callback(new Error('Not allowed by CORS'))
